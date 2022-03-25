07c7a20cf88cc5acdda1fb7d9ede6729ac008c70
Federico <federico.gibellini@mail.polimi.it>
2022-03-25 20:21:26 +0100

    Update Spark section
    Fixed almost all streaming tables for queries (only 1 still requires fixing)

--- Project1-Spark/src/main/java/it/polimi/mwtech2022/project1/DataAnalysis.java
+++ Project1-Spark/src/main/java/it/polimi/mwtech2022/project1/DataAnalysis.java
@@ -1,16 +1,25 @@
 package it.polimi.mwtech2022.project1;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import it.polimi.mwtech2022.project1.utils.LogUtils;
+import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.MapFunction;
 import org.apache.spark.sql.*;
+import org.apache.spark.sql.expressions.Window;
+import org.apache.spark.sql.expressions.WindowSpec;
 import org.apache.spark.sql.streaming.*;
+import scala.Tuple2;
 import scala.Tuple5;
 
 import java.sql.Date;
 import java.sql.Timestamp;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.TimeoutException;
+
+import static org.apache.spark.sql.functions.*;
 
 public class DataAnalysis {
 
@@ -23,9 +32,7 @@ public class DataAnalysis {
         final String socketHost = args.length > 1 ? args[1] : "localhost";
         final int socketPort = args.length > 2 ? Integer.parseInt(args[2]) : 9999;
         final String filePath = args.length > 3 ? args[3] : "./";
-        final String brokerUrl = args.length > 4 ? args[4] : "tcp://mqtt.neslab.it:3200";
-        final String topic = args.length > 5 ? args[5] : "gblgrlmnn/try/ciao";
-//        final int threshold = args.length > 4 ? Integer.parseInt(args[4]) : 50;
+        final int threshold = args.length > 4 ? Integer.parseInt(args[4]) : 75;
 
         final SparkSession spark = SparkSession
                 .builder()
@@ -33,7 +40,8 @@ public class DataAnalysis {
                 .appName("DataAnalysis")
                 .getOrCreate();
 
-        // requires inputs as pairs (POI, noise level)
+        // expected input (sample)
+        // {"noise":[42.662951293850554],"timestamp":1648141178047,"POIRegion":"Lazio","POIName":"Terme di Caracalla"}
 
         final Dataset<String> input = spark
                 .readStream()
@@ -45,36 +53,34 @@ public class DataAnalysis {
 //        input.printSchema();
 
         // may want to define splitting function aside (out of the map operator)
-        // works correctly with strings like "[string],[int]"
         // TODO needs check for cleaning purposes!!
-        // sent message (sample):
-        // {"noise":[42.662951293850554],"timestamp":1648141178047,"POIRegion":"Lazio","POIName":"Terme di Caracalla"}
-
+        // TODO: testing for actual noise array case
         final Dataset<Row> poiNoise = input
                 .as(Encoders.STRING())
-                .map((MapFunction<String, Tuple5<String, String, Timestamp, Date, Double>>) x -> {
+                .flatMap((FlatMapFunction<String, Tuple5<String, String, Timestamp, Long, Double>>) x -> {
                     Gson gson = new Gson();
                     JsonObject jsonRow = gson.fromJson(x, JsonObject.class);
-                    Tuple5<String, String, Timestamp, Date, Double> row;
+                    List<Tuple5<String, String, Timestamp, Long, Double>> rows = new LinkedList<>();
                     try {
                         String region = jsonRow.get("POIRegion").getAsString();
                         String name = jsonRow.get("POIName").getAsString();
-                        Timestamp ts = new Timestamp(jsonRow.get("timestamp").getAsLong());
-                        Date date = new Date(jsonRow.get("timestamp").getAsLong());
-                        Double noise = jsonRow.get("noise").getAsJsonArray().get(0).getAsDouble(); //at the moment only gets the first element in the array
-                        row = new Tuple5<>(name, region, ts, date, noise);
-                    } catch (Exception e){
+                        Long tsLong = jsonRow.get("timestamp").getAsLong();
+                        Timestamp ts = new Timestamp(tsLong);
+                        JsonArray noises = jsonRow.get("noise").getAsJsonArray();
+                        for (int i = 0; i < noises.size(); i++)
+                            rows.add(new Tuple5<>(name, region, ts, tsLong, noises.get(i).getAsDouble()));
+                    } catch (Exception e) {
+                        System.out.println("Exception thrown by FlatMap function"); // may be useful for debugging
                         e.printStackTrace();
-                        return null;
                     }
-                    return row;
-                }, Encoders.tuple(Encoders.STRING(), Encoders.STRING(), Encoders.TIMESTAMP(), Encoders.DATE(),Encoders.DOUBLE()))
-                .toDF("POIName", "POIRegion", "Timestamp", "Date", "Noise");
+                    return rows.iterator();
+                }, Encoders.tuple(Encoders.STRING(), Encoders.STRING(), Encoders.TIMESTAMP(), Encoders.LONG(), Encoders.DOUBLE()))
+                .toDF("POIName", "POIRegion", "Timestamp", "TSLong", "Noise");
 
         poiNoise.printSchema();
 
-//        poiNoise.withWatermark("timestamp", "1 hour");
-
+        // needs timestamp type column
+        poiNoise.withWatermark("timestamp", "1 hour");
 
 
 //        The backend periodically computes the following metrics:
@@ -87,58 +93,59 @@ public class DataAnalysis {
 //            ○ at any given point in time, the query should compute the point (or points) of interest with the
 //              longest stream.
 
-        //TODO: serious testing of these functions (the first one seems to work)
-        //TODO: need these functions with windows or related to specific hours/day/weeks?
-/*
+        // TODO: serious testing of these functions (the first one seems to work)
+        // testing OK
         final Dataset<Row> hourlyAverages = poiNoise
-                .groupBy(window(col("timestamp"), "1 hour", "5 minutes"),
-                            col("poi"))
-                .avg("noise-value");
+                .groupBy(window(col("Timestamp"), "1 hour", "5 minutes"),
+                        col("POIName"))
+                .avg("Noise");
         hourlyAverages.printSchema();
 
-        final Dataset<Row> lastHourAverages = poiNoise.
-                filter(col("timestamp").leq(new Timestamp(System.currentTimeMillis())))
-                .filter(col("timestamp").geq(new Timestamp(System.currentTimeMillis()-60*60*1000))) //millis in an hour
-                .groupBy(col("poi"))
-                .avg("noise-value")
-                .select(col("poi"), col("avg(noise-value)"));
+        //needs fixing
+//        final Dataset<Row> lastHour = hourlyAverages
+//                .select("window.end")
+//                .filter((col("window.end").cast("long")).leq(System.currentTimeMillis()))
+//                .orderBy(asc("window.end"));
+//                .limit(1);
 
-        final Dataset<Row> lastHourTop10 = lastHourAverages
-                .orderBy(col("avg(noise-value)"))
-                .limit(10);
+        // how to test this thing?? all the values I can get are in the last hour
+        // OK
+//        final Dataset<Row> lastHourAverages = hourlyAverages
+//                .filter(col("window").equals(lastHour.first().get(0)))
+//                .filter(col("TSLong").geq(System.currentTimeMillis() - 60 * 60 * 1000)) //millis in an hour
+//                .groupBy(col("POIName"))
+//                .avg("Noise");
+//                .select(col("POIName"), col("avg(Noise)"));
 
+//        final Dataset<Row> lastHourTop10 = lastHourAverages
+//                .orderBy(col("avg(noise-value)"))
+//                .limit(10);
+//
+        // testing OK
         final Dataset<Row> dailyAverages = poiNoise
-                .groupBy(window(col("timestamp"), "1 day", "1 hour"),
-                        col("poi"))
-                .avg("noise-value");
+                .groupBy(window(col("Timestamp"), "1 day", "1 hour"),
+                        col("POIName"))
+                .avg("Noise");
 
+
+        // testing OK
         final Dataset<Row> weeklyAverages = poiNoise
-                .groupBy(window(col("timestamp"), "1 week", "1 day"),
-                        col("poi"))
-                .avg("noise-value");
+                .groupBy(window(col("Timestamp"), "1 week", "1 day"),
+                        col("POIName"))
+                .avg("Noise");
 
-        final Timestamp lastTS = (Timestamp) poiNoise
-                .orderBy("timestamp")
-                .first().get(0);
-
-        // TODO: streak seems not to get updated --> this part needs fixing
+        // testing OK
         final Dataset<Row> lastTimeOverThreshold = poiNoise
-                .filter(col("noise-value").geq(threshold))
-                .select("timestamp", "poi")
-                .as(Encoders.tuple(Encoders.TIMESTAMP(), Encoders.STRING()))
-                .map((MapFunction<Tuple2<Timestamp, String>, Tuple2<String, Long>>) t -> {
-                    long tsDiff = (new Timestamp(System.currentTimeMillis())).getTime() - t._1.getTime();
-                    return new Tuple2<>(t._2, tsDiff);
-                }, Encoders.tuple(Encoders.STRING(), Encoders.LONG()))
-                .toDF("poi", "streak")
-                .groupBy("poi")
-                .min("streak");
-*/
+                .filter(col("Noise").geq(threshold))
+                .groupBy("POIName")
+                .max("TSLong")
+                .orderBy(asc("max(TSLong)"))
+                .limit(1);
 
-        final StreamingQuery query = poiNoise
+        final StreamingQuery query = lastTimeOverThreshold
                 .writeStream()
                 .format("console")
-                .outputMode("update")
+                .outputMode("complete")
                 .start();
 
         try {
@@ -149,7 +156,6 @@ public class DataAnalysis {
 
         spark.close();
     }
-
 
 
 }
